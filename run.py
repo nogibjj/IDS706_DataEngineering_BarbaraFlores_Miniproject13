f4bbc50428b4ac56e4f01c080ed647b4ca26c427
Barbara Flores Rios <143648839+BarbaraPFloresRios@users.noreply.github.com>
2023-12-09 01:07:47 +0000

    update

--- run.py
+++ run.py
@@ -1,31 +1,22 @@
-from app import app
-
-if __name__ == '__main__':
-    app.run(debug=True)
-from flask import Flask, render_template, request
-import openai
+from flask import Flask, render_template
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
 app = Flask(__name__)
-openai.api_key = 'TU_API_KEY'  # Reemplaza con tu clave de API de OpenAI
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    # Cargar modelo y tokenizador
+    model = GPT2LMHeadModel.from_pretrained("gpt2")
+    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 
-@app.route('/gpt3', methods=['POST'])
-def gpt3():
-    if request.method == 'POST':
-        prompt = request.form['prompt']
-        response = generate_response(prompt)
-        return render_template('index.html', prompt=prompt, response=response)
+    # Generar texto con el modelo
+    input_text = "Escribe aquí tu texto de entrada..."
+    input_ids = tokenizer.encode(input_text, return_tensors="pt")
+    output = model.generate(input_ids, max_length=50, num_beams=5, no_repeat_ngram_size=2, top_k=50, top_p=0.95, temperature=0.7)
 
-def generate_response(prompt):
-    response = openai.Completion.create(
-        engine="text-davinci-002",  # Puedes elegir otro motor según tus necesidades
-        prompt=prompt,
-        max_tokens=100  # Ajusta según sea necesario
-    )
-    return response.choices[0].text.strip()
+    # Decodificar y renderizar el resultado
+    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+    return render_template('index.html', generated_text=generated_text)
 
 if __name__ == '__main__':
     app.run(debug=True)
